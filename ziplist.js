86bf186738faea7926d45615a73d045f776c16c5
kyho22 <kyho35@hawaii.edu>
2018-02-05 13:54:09 -1000

    Finished PWOD2

---
+++ ziplist.js
@@ -0,0 +1,19 @@
+const alist = [1, 2, 3];
+const blist = ['a', 'b', 'c'];
+
+function zipList(list1, list2) {
+  const newArray = [];
+  for (let i = 0; i < list1.length; i++) {
+    newArray.push(list1[i]);
+    newArray.push(list2[i]);
+  }
+  return newArray;
+}
+
+console.log(zipList(alist, blist));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(alist, blist));
